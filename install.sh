07db334c738c85c6298004cf27a625c3c55ae69f
gpr2109 <ragavangp12b320.2122@gmail.com>
2021-10-27 07:29:25 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Make sure you change it to your cloning directory to avoid error
+CLONE_DIR=/home/ragavan/git/qtile-setup
+
+ln -s $CLONE_DIR/awesome ~/.config/ &
+ln -s $CLONE_DIR/macchina ~/.config/ &
+ln -s $CLONE_DIR/.Xresources ~/ &
+ln -s $CLONE_DIR/sx ~/.config/ &
+ln -s $CLONE_DIR/rofi ~/.config/ &
+ln -s $CLONE_DIR/.vimrc ~/ &
