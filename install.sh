73e996fb91d58b1f2d2dbc661bf823bae515ab3c
gpr2109 <ragavangp12b320.2122@gmail.com>
2021-10-29 06:43:07 +0530

    Tweaked the installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-TARGET_DIR=~/.config/
+TARGET_DIR=~
 
-stow vim &
-stow macchina
-stow Xresources &
-stow awesome -t $TARGET_DIR &
-stow rofi -t $TARGET_DIR &
-stow sx -t $TARGET_DIR &
+stow vim -t $TARGET_DIR &
+stow macchina -t $TARGET_DIR &
+stow Xresources -t $TARGET_DIR &
+stow awesome -t $TARGET_DIR/.config/ &
+stow rofi -t $TARGET_DIR/.config/ &
+stow sx -t $TARGET_DIR/.config/ &
 
